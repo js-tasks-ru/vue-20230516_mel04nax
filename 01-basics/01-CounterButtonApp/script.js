d57e48cb3d94bf85f8bbc174060cc71727e2daac
synt3tic <mel04nax@gmail.com>
2023-05-22 16:38:26 +0300

    counter-button-app

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,19 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const App = defineComponent({
+  name: 'App',
+
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    incrementCounter() {
+      this.counter++;
+    },
+  },
+});
+
+const app = createApp(App);
+app.mount('#app');
